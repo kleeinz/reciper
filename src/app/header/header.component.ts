b00e11f56ae6bc5619d29403da559558740cd886
Christian Omar Lopez Trejo <kleeinzz@gmail.com>
2017-10-16 19:48:12 -0500

    Continuando con el curso de max

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+
+
+  public onSelect(feature: string) {
+    console.log(feature);
+    this.featureSelected.emit(feature);
+  }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,6 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is simply a test',
       'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
-
   ];
 
   constructor() { }
